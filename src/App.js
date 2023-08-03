19588082931d6b69063479b3fedb028ac56b195f
Himanshi Rawat <himanshirawat604@gmail.com>
2023-08-03 14:07:19 +0530

    components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import {
+  Home,
+  SingleHotel,
+  SearchResults,
+  Wishlist,
+  Payment,
+  OrderSummary
+} from "./pages";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App"> <h1>Hello</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/hotels/:name/:address/:id/reserve"
+        element={<SingleHotel />}
+      />
+      <Route path="/hotels/:address" element={<SearchResults />} />
+      <Route path="/wishlists" element={<Wishlist />} />
+      <Route path="/confirm-booking/stay/:id" element={<Payment />} />
+      <Route path="/order-summary" element={<OrderSummary />} />
+    </Routes>
   );
 }
 
