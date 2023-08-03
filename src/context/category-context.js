901d76a1a6068aee498a58503a8d9cbcbcadc346
Himanshi Rawat <himanshirawat604@gmail.com>
2023-08-03 15:29:22 +0530

    context created

---
+++ src/context/category-context.js
@@ -0,0 +1,15 @@
+import { createContext, useContext, useState } from "react";
+
+const initialValue = "National Parks";
+
+const CategoryContext = createContext(initialValue);
+
+const CategoryProvider = ({ children }) => {
+  const [hotelCategory, setHotelCategory] = useState(initialValue);
+
+  return (
+    <CategoryContext.Provider value={{ hotelCategory, setHotelCategory }}>
+      {children}
+    </CategoryContext.Provider>
+  );
+};
